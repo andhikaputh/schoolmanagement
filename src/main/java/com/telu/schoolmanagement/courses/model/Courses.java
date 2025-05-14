d2014fa129c689aecca5dbb5b678817f2597054e
Reggi Fachri <rfachri.exe@gmail.com>
2025-05-14 16:56:13 +0700

    fix(courses):follow variable naming guideline for backend

--- src/main/java/com/telu/schoolmanagement/courses/model/Courses.java
+++ src/main/java/com/telu/schoolmanagement/courses/model/Courses.java
@@ -1,5 +1,6 @@
 package com.telu.schoolmanagement.courses.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,13 +24,17 @@ public class Courses {
     private int sks;
 
     //    refer to programs table
-    private int program_id;
+    @Column(name = "program_id")
+    private int programId;
 
     //    refer to users table
-    private int created_by;
-    private int updated_by;
+    @Column(name = "created_by")
+    private int createdBy;
+    @Column(name = "updated_by")
+    private int updatedBy;
 
-    private LocalDateTime created_at;
-    private LocalDateTime updated_at;
-
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
