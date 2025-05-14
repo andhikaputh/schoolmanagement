8109b0d3d2e331bed8e120733677b6b3bad85a48
9William <100459420+9William@users.noreply.github.com>
2025-05-14 17:20:55 +0700

    feat(users): Update users

--- src/main/java/com/telu/schoolmanagement/users/controller/UsersController.java
+++ src/main/java/com/telu/schoolmanagement/users/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.telu.schoolmanagement.users.controller;
 import com.telu.schoolmanagement.common.response.ApiResponse;
 import com.telu.schoolmanagement.users.dto.UsersResponseDTO;
 import com.telu.schoolmanagement.users.service.UsersService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,40 +24,40 @@ public class UsersController {
     @Autowired
     private UsersService usersService;
 
-    @Tag(name = "Get all Users Data")
+    @Operation(summary = "Get all Users Data")
     @GetMapping
     public ResponseEntity<ApiResponse<List<UsersResponseDTO>>> getAllUsers() {
         return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getAllUsers()));
     }
 
-//    @Tag(name = "Get Users by Id")
-//    @GetMapping("/id={id}")
-//    public ResponseEntity<ApiResponse<UsersResponseDTO>> getIdUsers(@PathVariable Long id) {
-//        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUsersById(id)));
-//    }
-//
-//    @Tag(name = "Get Users by Nip")
-//    @GetMapping("/nip={nip}")
-//    public ResponseEntity<ApiResponse<UsersResponseDTO>> getUsersByNip(@PathVariable String nip) {
-//        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUsersByNip(nip)));
-//    }
-//    @Tag(name = "Get Users by Name")
-//    @GetMapping("/name={name}")
-//    public ResponseEntity<ApiResponse<List<UsersResponseDTO>>> getUsersByName(@PathVariable String name) {
-//        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUserByName(name)));
-//    }
-//
-//    @Tag(name = "Get Users if active")
-//    @GetMapping("/act={act}")
-//    public ResponseEntity<ApiResponse<List<UsersResponseDTO>>> getUsersByIsAct(@PathVariable Boolean act) {
-//        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUserByIsAct(act)));
-//    }
-//
-//    @Tag(name = "Get Users Graduated date")
-//    @GetMapping("/grad={grad}")
-//    public ResponseEntity<ApiResponse<List<UsersResponseDTO>>> getUsersByGrad(@PathVariable("grad") String grad) {
-//        LocalDate date = LocalDate.parse(grad);
-//        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUsersByGrad(date)));
-//    }
+    @Operation(summary = "Get Users by Id")
+    @GetMapping("/id={id}")
+    public ResponseEntity<ApiResponse<UsersResponseDTO>> getIdUsers(@PathVariable Long id) {
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUsersById(id)));
+    }
+
+    @Operation(summary = "Get Users by Nip")
+    @GetMapping("/nip={nip}")
+    public ResponseEntity<ApiResponse<UsersResponseDTO>> getUsersByNip(@PathVariable String nip) {
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUsersByNip(nip)));
+    }
+    @Operation(summary = "Get Users by Name")
+    @GetMapping("/name={name}")
+    public ResponseEntity<ApiResponse<List<UsersResponseDTO>>> getUsersByName(@PathVariable String name) {
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUserByName(name)));
+    }
+
+    @Operation(summary = "Get Users if active")
+    @GetMapping("/act={act}")
+    public ResponseEntity<ApiResponse<List<UsersResponseDTO>>> getUsersByIsAct(@PathVariable Boolean act) {
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUserByIsAct(act)));
+    }
+
+    @Operation(summary = "Get Users Graduated date")
+    @GetMapping("/grad={grad}")
+    public ResponseEntity<ApiResponse<List<UsersResponseDTO>>> getUsersByGrad(@PathVariable("grad") String grad) {
+        LocalDate date = LocalDate.parse(grad);
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", usersService.getUsersByGrad(date)));
+    }
 
 }
