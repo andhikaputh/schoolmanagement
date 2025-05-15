b55343431427effa2511ecc2ca529a389e9673d3
Reggi Fachri <reggifachri@lgsinarmas.com>
2025-05-15 17:11:44 +0700

    chore(courses):add request/responseDTO

--- src/main/java/com/telu/schoolmanagement/courses/dto/CoursesRequestDTO.java
+++ src/main/java/com/telu/schoolmanagement/courses/dto/CoursesRequestDTO.java
@@ -1,4 +1,7 @@
 package com.telu.schoolmanagement.courses.dto;
 
 public class CoursesRequestDTO {
+    private String name;
+    private int sks;
+    private Long programId;
 }
--- src/main/java/com/telu/schoolmanagement/courses/dto/CoursesResponseDTO.java
+++ src/main/java/com/telu/schoolmanagement/courses/dto/CoursesResponseDTO.java
@@ -1,5 +1,21 @@
 package com.telu.schoolmanagement.courses.dto;
 
-public class CoursesResponseDTO {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoursesResponseDTO {
+    private Long id;
+    private String name;
+    private int sks;
+    private int programId;
+    private Long createdBy;
+    private Long updatedBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
