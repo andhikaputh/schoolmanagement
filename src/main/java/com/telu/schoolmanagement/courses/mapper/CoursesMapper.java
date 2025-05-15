f3f5f1cb451123d1bcaffd4885fdf370059fef58
Reggi Fachri <reggifachri@lgsinarmas.com>
2025-05-15 17:12:23 +0700

    chore(courses):add mapper

--- src/main/java/com/telu/schoolmanagement/courses/mapper/CoursesMapper.java
+++ src/main/java/com/telu/schoolmanagement/courses/mapper/CoursesMapper.java
@@ -1,5 +1,20 @@
 package com.telu.schoolmanagement.courses.mapper;
 
+import com.telu.schoolmanagement.courses.dto.CoursesResponseDTO;
+import com.telu.schoolmanagement.courses.model.Courses;
+
 public class CoursesMapper {
 
+    public static CoursesResponseDTO toDTO(Courses courses) {
+        return CoursesResponseDTO.builder()
+                .id(courses.getId())
+                .name(courses.getName())
+                .sks(courses.getSks())
+                .programId(courses.getProgramId())
+                .createdBy(courses.getCreatedBy())
+                .updatedBy(courses.getUpdatedBy())
+                .createdAt(courses.getCreatedAt())
+                .updatedAt(courses.getUpdatedAt())
+                .build();
+    }
 }
