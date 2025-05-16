2fb1c240e68094f2b9786144c8c8deb79f798114
Reggi Fachri <reggifachri@lgsinarmas.com>
2025-05-16 16:40:53 +0700

    fix(courses):modify getCourseByID to searchCourses(id,name)

--- src/main/java/com/telu/schoolmanagement/courses/controller/CoursesController.java
+++ src/main/java/com/telu/schoolmanagement/courses/controller/CoursesController.java
@@ -4,15 +4,14 @@ import com.telu.schoolmanagement.common.response.ApiResponse;
 import com.telu.schoolmanagement.courses.dto.CoursesResponseDTO;
 import com.telu.schoolmanagement.courses.service.CoursesService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Courses", description = "CRUD for Courses")
 @RestController
 @RequestMapping("/api/courses")
 public class CoursesController {
@@ -26,12 +25,12 @@ public class CoursesController {
         return ResponseEntity.ok(new ApiResponse<>(true, "Success", coursesService.getAllCourses()));
     }
 
-    @Operation(summary = "Get course by id")
-    @GetMapping("/id={id}")
-    public ResponseEntity<ApiResponse<CoursesResponseDTO>> getCourseById(@PathVariable Long id) {
-        CoursesResponseDTO course = coursesService.getCourseById(id);
-        if (course != null) {
-            return ResponseEntity.ok(new ApiResponse<>(true, "Success", course));
+    @Operation(summary = "Search course by id or name")
+    @GetMapping("/search")
+    public ResponseEntity<ApiResponse<List<CoursesResponseDTO>>> searchCourses(@RequestParam(required = false) Long id, @RequestParam(required = false) String name) {
+        List<CoursesResponseDTO> courses = coursesService.searchCourses(id, name);
+        if (courses != null) {
+            return ResponseEntity.ok(new ApiResponse<>(true, "Success", courses));
         } else {
             return ResponseEntity.status(404).body(new ApiResponse<>(false, "Course not found", null));
         }
--- src/main/java/com/telu/schoolmanagement/courses/service/CoursesService.java
+++ src/main/java/com/telu/schoolmanagement/courses/service/CoursesService.java
@@ -3,7 +3,6 @@ package com.telu.schoolmanagement.courses.service;
 import com.telu.schoolmanagement.courses.dto.CoursesResponseDTO;
 import com.telu.schoolmanagement.courses.mapper.CoursesMapper;
 import com.telu.schoolmanagement.courses.repository.CoursesRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +25,20 @@ public class CoursesService {
         return coursesRepository.findById(id)
                 .map(CoursesMapper::toDTO)
                 .orElse(null);
+    }
+
+    public List<CoursesResponseDTO> searchCourses(Long id, String name) {
+        if (id != null) {
+            return coursesRepository.findById(id)
+                    .stream()
+                    .map(CoursesMapper::toDTO)
+                    .toList();
+        } else if (name != null) {
+            return coursesRepository.findByNameIgnoreCaseContaining(name)
+                    .stream()
+                    .map(CoursesMapper::toDTO)
+                    .toList();
+        }
+        return null;
     }
 }
