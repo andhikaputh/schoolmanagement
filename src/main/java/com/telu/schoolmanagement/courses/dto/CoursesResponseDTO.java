87effba36d062e70522edc6f6b604c9967d8da98
Reggi Fachri <reggifachri@lgsinarmas.com>
2025-05-19 11:48:04 +0700

    feat(courses):add slug column and handle it

--- src/main/java/com/telu/schoolmanagement/courses/dto/CoursesResponseDTO.java
+++ src/main/java/com/telu/schoolmanagement/courses/dto/CoursesResponseDTO.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class CoursesResponseDTO {
     private Long id;
     private String name;
+    private String slug;
     private int sks;
     private int programId;
     private Long createdBy;
