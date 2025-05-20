5008f57554fa7ea8a2b905b6a6aa6ef61ed1108d
Aa Maulana <aamaulanabola10@gmail.com>
2025-05-20 10:48:30 +0700

    feat(swagger): adding swagger config

---
+++ src/main/java/com/telu/schoolmanagement/common/swagger/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.telu.schoolmanagement.common.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "Authorization";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("School Management API")
+                        .version("v1")
+                        .description("Documentation for School Management System"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER))
+                );
+    }
+}
