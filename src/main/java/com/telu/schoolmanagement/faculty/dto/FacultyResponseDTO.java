ed25b0a505f46cc5ae8d0d092c6f3d35ba1f23ef
Andhika Putra <andhikapasagi55@gmail.com>
2025-05-21 13:48:47 +0700

    feat(faculty): adding redis

--- src/main/java/com/telu/schoolmanagement/faculty/dto/FacultyResponseDTO.java
+++ src/main/java/com/telu/schoolmanagement/faculty/dto/FacultyResponseDTO.java
@@ -6,13 +6,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class FacultyResponseDTO {
+public class FacultyResponseDTO implements Serializable {
     private Long id;
     private String facultyName;
     private GeneralCreatedUpdatedBy createdBy;
