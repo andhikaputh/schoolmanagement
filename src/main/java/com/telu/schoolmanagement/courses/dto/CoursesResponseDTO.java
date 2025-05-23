973a8c5196fcb26adfc794f97d91e88381d1540a
Reggi Fachri <reggifachri@lgsinarmas.com>
2025-05-23 21:08:02 +0700

    fix(courses):implement object relation

--- src/main/java/com/telu/schoolmanagement/courses/dto/CoursesResponseDTO.java
+++ src/main/java/com/telu/schoolmanagement/courses/dto/CoursesResponseDTO.java
@@ -6,20 +6,33 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CoursesResponseDTO {
+public class CoursesResponseDTO implements Serializable {
     private Long id;
     private String name;
     private String slug;
     private int sks;
-    private int programId;
+
+    //TODO change to ProgramResponseDTO after program merged to master
+    private ProgramSummary program;
     private GeneralCreatedUpdatedBy createdBy;
     private GeneralCreatedUpdatedBy updatedBy;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ProgramSummary {
+        private Long id;
+        private String name;
+    }
 }
+
