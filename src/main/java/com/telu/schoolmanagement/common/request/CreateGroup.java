be19a570a15711d4fe9f0ae71e537502d0ae8a2e
Reggi Fachri <reggifachri@lgsinarmas.com>
2025-05-25 20:58:11 +0700

    feat(course):add request body validation

---
+++ src/main/java/com/telu/schoolmanagement/common/request/CreateGroup.java
@@ -0,0 +1,4 @@
+package com.telu.schoolmanagement.common.request;
+
+public interface CreateGroup {}
+
--- src/main/java/com/telu/schoolmanagement/courses/dto/CoursesRequestDTO.java
+++ src/main/java/com/telu/schoolmanagement/courses/dto/CoursesRequestDTO.java
@@ -1,20 +1,32 @@
 package com.telu.schoolmanagement.courses.dto;
 
+import com.telu.schoolmanagement.common.request.CreateGroup;
+import com.telu.schoolmanagement.common.request.UpdateGroup;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CoursesRequestDTO {
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @Min(value= 1, message = "Minimum SKS is 1")
     private int sks;
+
+    @NotNull(message = "Program ID cannot be null")
     private Long programId;
+
+    @NotNull(message = "Created By cannot be null", groups = CreateGroup.class)
     private Long createdBy;
+
+    @NotNull(message = "Updated By cannot be null", groups = UpdateGroup.class)
     private Long updatedBy;
 }
